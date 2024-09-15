7d5a1aeceb20f80b2a80036fe53bdc6b761aa88e
jcneves-philips <jean.neves@philips.com>
2024-09-15 17:17:22 -0300

    First Java File Project

---
+++ firstProject/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/spring_boot/StartUpTests.java
@@ -0,0 +1,13 @@
+package br.com.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
